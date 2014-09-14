1bcd5b986e5869194553f3461239eba33fa51066
Baptiste Mispelon <bmispelon@gmail.com>
2014-09-14 02:55:41 +0200

    Added some comments to the modified loaddata.

--- pyxiedust/utils/management/commands/loaddata2.py
+++ pyxiedust/utils/management/commands/loaddata2.py
@@ -4,7 +4,13 @@ from django.core.management.commands.loaddata import Command as LoadDataCommand
 
 
 class Command(LoadDataCommand):
+    """
+    A quick hack to support loading data from stdin, using the filename '-'.
+    When doing so, json format is assumed.
+    """
     def parse_name(self, fixture_name):
+        # Hack the compression formats to include a 'stdin' format that
+        # will read from sys.stdin
         self.compression_formats['stdin'] = (lambda x,y: sys.stdin, None)
         if fixture_name == '-':
             return '-', 'json', 'stdin'
